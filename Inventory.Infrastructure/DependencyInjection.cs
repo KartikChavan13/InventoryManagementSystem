a19307e4e9728b514dd27761f21932cfcf764dcb
kartik13-kkok <chavankartik048@gmail.com>
2026-06-28 15:42:03 +0530

    Configured Infrastructure Dependency Injection

---
+++ Inventory.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
